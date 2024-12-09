68b2e578907feaec192d0907fe091770e6a6d8bf
largenumberhere <largeNumbergoeshere@gmail.com>
2024-12-09 20:26:29 +1100

    moved some functions to aoc_lib module

--- aoc_lib/aoc.go
+++ aoc_lib/aoc.go
@@ -1,11 +1,104 @@
 package libaoc
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
 
-func TestFunc() {
-	fmt.Println("hello world!")
+/*
+ *	Read the aoc file and remove any spurious whitespace
+ */
+func GrabInput(path string) (string, error) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	str := string(bytes)
+
+	// remove trailing and leading whitespace
+	str = strings.Trim(str, " \n\r\t")
+
+	// remove pesky carriage returns
+	str = strings.Replace(str, "\r", "", -1)
+
+	return str, nil
 }
 
-func testFunc2() {
-	fmt.Println("nuh uh")
+/*
+ *	Parse the lines of an aoc file
+ */
+func GrabLines(path string) ([]string, error) {
+	file_contents, err := GrabInput(path)
+	if err != nil {
+		return []string{}, err
+	}
+
+	var lines = strings.Split(file_contents, "\n")
+
+	return lines, nil
+}
+
+/*
+ *	Print a 2d array of runes
+ */
+func PrintRunes(runes [][]rune) {
+	for _, i := range runes {
+		for _, j := range i {
+			if unicode.IsPrint(j) {
+				fmt.Print(string(j))
+			} else {
+				fmt.Print(" ", j, " ")
+			}
+		}
+		fmt.Println("")
+	}
+
+}
+
+/*
+ *	Parse an aoc file into a 2d array of runes
+ */
+func GrabRunesArray(path string) ([][]rune, error) {
+	var lines_runes [][]rune
+
+	lines, err := GrabLines(path)
+	if err != nil {
+		return [][]rune{}, err
+	}
+
+	for _, line := range lines {
+		var line_rune []rune
+		for _, rune := range line {
+			if rune == '\r' {
+				// ignore pesky carriage returns
+				continue
+			}
+			line_rune = append(line_rune, rune)
+		}
+
+		lines_runes = append(lines_runes, line_rune)
+	}
+
+	return lines_runes, nil
+}
+
+/*
+ *	Initalize a 2d array of runes
+ */
+func MakeRunes(rows int, columns int, initialized_value rune) [][]rune {
+	runes := make([][]rune, rows)
+	for i := 0; i < rows; i++ {
+		runes[i] = make([]rune, columns)
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			runes[i][j] = initialized_value
+		}
+	}
+
+	return runes
 }
